1c7125d00f168efa4bed06209943c2b5737a9d3e
Arkkro <djarkkro@gmail.com>
2017-03-28 20:02:09 -0700

    m

---
+++ search.js
@@ -0,0 +1,15 @@
+var input = document.getElementById('search')
+input.addEventListener(onsubmit, search)
+
+function search() {
+  console.log("Passed");
+
+    // var win = window.open('http://stackoverflow.com/', '_blank');
+    // if (win) {
+    //     //Browser has allowed it to be opened
+    //     win.focus();
+    // } else {
+    //     //Browser has blocked it
+    //     alert('Please allow popups for this website');
+    // }
+}
