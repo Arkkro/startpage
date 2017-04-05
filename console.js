1c401f6e14a0e977ca64b707f5a1606bd725a223
Arkkro <djarkkro@gmail.com>
2017-04-05 11:42:34 -0700

    More Js

--- console.js
+++ console.js
@@ -1,9 +1,13 @@
 var term = document.getElementById("consoleinput");
+var termbox = document.getElementById("consoleinputbox");
 var log = document.getElementById("log");
 var user = document.getElementById("user");
 var mainbox = document.getElementById("main1");
 var initLog = log;
-var userList = [];
+var brCount = 0;
+programList = ["help", "clear", "programList", "plp", "amazon", "tumblr", "dA", "/ic/"];
+
+// var userList = [];
 
 
 user = user.value = ">";
@@ -15,6 +19,10 @@ function addLog() {
         // alert("anything, doesn't matter");
     }
 }
+function scaleTerm() {
+    var scale = brCount *14;
+    log.style.height(500 + scale);
+}
 // function ex
 // function readCookie() {
 //   document.cookie
@@ -22,29 +30,57 @@ function addLog() {
 
 function execCommand() {
     // console.log(term.value);
-    if (command == "clear") {
+    if (command == "") {
+        commandLog = user + "<br>"
+        brCount = brCount + 1;
+    } else if (command == "clear") {
         log.innerHTML = "";
-    } else if (command == "programList") {
-        commandLog = user + " " + "help" + "<br>" + user + " " + "clear" + "<br>"
+    } else if (command == "ls") {
+        // commandLog = user + " " + "help" + "<br>" + user + " " + "clear" + "<br>"
+        commandLog = "";
+        for (var i = 0; i < programList.length; i++) {
+            var commandLogAdd = user + " " + "\"" + programList[i] + "\"" + "<br>"
+            commandLog = commandLog + commandLogAdd;
+        }
+        brCount = brCount + programList.length;
+    } else if (command == "help") {
+        commandLog = user + " " + "Use the command \"ls\" to list all commands" + "<br>" + user + " Use the command \"login (username)\" to set the user" + "<br>";
+        brCount = brCount + 2;
+    } else if (command.includes("login")) {
+        user = "[" + command.slice(6) + "@startpage]$";
+        console.log(command);
+        loggedIn = true;
+        commandLog = user + "<br>";
+        brCount = brCount + 1;
+
     } else if (command == "plp") {
         window.open("https://app.mysummitps.org");
         commandLog = user + " " + command + "<br>"
+        brCount = brCount + 1;
+    } else if (command == "amazon") {
+        window.open("https://www.amazon.com");
+        commandLog = user + " " + command + "<br>"
+        brCount = brCount + 1;
     } else if (command == "youtube") {
         window.open("https://youtube.com");
         commandLog = user + " " + command + "<br>"
-
+        brCount = brCount + 1;
     } else if (command == "tumblr") {
         window.open("https://tumblr.com/dashboard")
         commandLog = user + " " + command + "<br>"
+        brCount = brCount + 1;
     } else if (command == "dA") {
         window.open("https://deviantart.com")
         commandLog = user + " " + command + "<br>"
+        brCount = brCount + 1;
     } else if (command == "ic") {
         window.open("https://boards.4chan.org/ic/")
         commandLog = user + " " + command + "<br>"
+        brCount = brCount + 1;
     } else if (command == "commandName") {
         window.open("link")
         commandLog = user + " " + command + "<br>"
+        brCount = brCount + 1;
     }
     // else if (command = "commandName") {
     //   window.open("link")
@@ -65,11 +101,15 @@ function execCommand() {
     }
 }
 term.addEventListener("keydown", function(evt) {
-        if (evt.keyCode === 13) {
-            command = term.value;
-            execCommand();
-            addLog();
-            term.value = "";
+    if (evt.keyCode === 13) {
+        command = term.value;
+        execCommand();
+        addLog();
+        if (brCount >= 7) {
+          scaleTerm();
+          // alert("test");
         }
-    })
-    // consoletext.innerHTML =
+        term.value = "";
+    }
+})
+// consoletext.innerHTML =
